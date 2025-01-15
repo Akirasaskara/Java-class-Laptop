49d9a6772522958e7d38615ab14375f8cfa7a4ec
Akira <hartonoakira310@gmail.com>
2025-01-15 14:00:05 +0700

    Add files via upload

---
+++ LAPTOP/JenisLaptop.java
@@ -0,0 +1,17 @@
+package LAPTOP;
+
+public class JenisLaptop {
+
+    String merkLaptop = "Asus ROG";
+    String jenisLaptop = "Laptop Gaming";
+    String warna = "Hitam";
+    String spek = "Intel Core pentium";
+
+    void tampilnamalaptop() {
+        System.out.println("merkLaptop = " + merkLaptop);
+        System.out.println("jenisLaptop = " + jenisLaptop);
+        System.out.println("warna = " + warna);
+        System.out.println("spek = " + spek);
+    }
+
+}
---
+++ LAPTOP/ObjekLaptop.java
@@ -0,0 +1,9 @@
+package LAPTOP;
+
+public class ObjekLaptop {
+
+    public static void main(String[] args) {
+        JenisLaptop j = new JenisLaptop();
+        j.tampilnamalaptop(); //
+    }
+}
